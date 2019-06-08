f25d6ae1e30d647944ffb5fca2c0d78b84011fc9
Duncan Kirby <39995281+dakirby@users.noreply.github.com>
2019-06-08 13:59:25 -0400

    Fixed whitespace parsing in hsdn.py

--- plot_disease_network.py
+++ plot_disease_network.py
@@ -1,9 +1,25 @@
 import numpy as np
+import os
+import pickle
 from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+
+# ----------------------------------------------
+# Globals
+# ----------------------------------------------
+disease_labels = []
+symptom_features = []
+hsdn = []
+with open(os.path.join(os.getcwd(), 'databases', 'disease_labels.p'), 'rb') as f:
+    disease_labels = pickle.load(f)
+with open(os.path.join(os.getcwd(), 'databases', 'symptom_features.p'), 'rb') as f:
+    symptom_features = pickle.load(f)
+with open(os.path.join(os.getcwd(), 'databases', 'hsdn.p'), 'rb') as f:
+    hsdn = pickle.load(f)
 
 
 if __name__ == "__main__":
-    disease_symptom_matrix = build_disease_symptom_matrix()
-    print(np.shape(disease_symptom_matrix))
-    with open(os.path.join(os.getcwd(), 'databases', 'hsdn.p'), 'wb') as f:
-        pickle.dump(disease_symptom_matrix, f)
+    """
+    X_embedded = TSNE(n_components=2).fit_transform(X)
+    
+    """
